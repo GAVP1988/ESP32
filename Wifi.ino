7f449931f0b7bb8d0fa5ddd58f4ecdf53c1edf88
Guilherme <gavp1988@gmail.com>
2022-09-06 09:27:02 +0200

    File commited

--- Wifi.ino
+++ Wifi.ino
@@ -5,6 +5,14 @@ const char *ssid = "KNUET-Gast";
 const char *password = "Knuet201";
 WiFiClient espClient;
 
+const char *mqttBrocker = "broker.emqx.io";
+const char *topic = "esp32/test";
+const char *mqttUsername = "emqx";
+const char *mqttPassword = "public";
+const int mqttPort = 1883;
+PubSubClient client(espClient);
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,9 +24,42 @@ void setup() {
     Serial.println(ssid);
   }
   Serial.println("Connection done.");
+  client.setServer(mqttBrocker, mqttPort);
+  client.setCallback(callback);
+  while(!client.connected()){
+    String clientID = "Guilherme";
+    clientID += String(WiFi.macAddress());
+    Serial.printf("The %s tries to connect to mqtt borcker...\n",clientID.c_str());
+    if(client.connect(clientID.c_str(), mqttUsername, mqttPassword)){
+      Serial.println("mqtt brocker connected");
+    }
+    else {
+      Serial.print("mqtt connection failed");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
+
+  client.publish(topic, "Hi EMQX brocker I'm a ESP32 :)");
+  client.subscribe(topic);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  client.loop();
+}
 
+void callback(char *topic, byte *payload, unsigned int length){
+  
+  Serial.print("Message received in topic: ");
+  Serial.println(topic);
+  Serial.print("The message is: ");
+  
+  for(int i=0;i<length; i++){
+    Serial.print((char) payload[i]);
+  }
+  
+  Serial.println();
+  Serial.println("-+-+-+End+-+-+-+");
 }
